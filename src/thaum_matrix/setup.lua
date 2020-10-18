69cd5d155f1fc15ecfd1a400e90a7aa8cbcbadeb
Alexander Sokol <sokol.kh@gmail.com>
2020-10-19 00:42:08 +0300

    fix recipe indexes

--- src/thaum_matrix/setup.lua
+++ src/thaum_matrix/setup.lua
@@ -1,5 +1,17 @@
+local shell = require("shell")
+local filesystem = require("filesystem")
+
+filesystem.remove("/home/aspect.lua")
 shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/aspect/aspect.lua /home/aspect.lua")
+
+filesystem.remove("/home/crafting_state.lua")
 shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/crafting_state.lua /home/crafting_state.lua")
+
+filesystem.remove("/home/master.lua")
 shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/master.lua /home/master.lua")
+
+filesystem.remove("/home/options.lua")
 shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/options.lua /home/options.lua")
+
+filesystem.remove("/home/recipes.lua")
 shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/recipes.lua /home/recipes.lua")
