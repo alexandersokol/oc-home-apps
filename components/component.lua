7a686ec76a98293346d9a5cff7af1ca5331e11e2
Alexander Sokol <sokol.a@dbbest.com>
2020-10-18 17:59:28 +0300

    add component mocks

---
+++ components/component.lua
@@ -0,0 +1,19 @@
+local component = {}
+
+component.inventory_controller = require("inventory_controller")
+component.tractor_beam = require("tractor_beam")
+component.tank_controller = require("tank_controller")
+component.redstone = require("redstone")
+component.data = require("data")
+component.modem = require("modem")
+component.gpu = require("gpu")
+component.gt_machine = require("gt_machine")
+component.transposer = require("transposer")
+component.me_interface = require("me_interface")
+component.database = require("database")
+
+function component.isAvailable(componentType)
+
+end
+
+return component
