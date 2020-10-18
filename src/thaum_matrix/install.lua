62d2dad1fcb1934a2d3fc2d30c127003ec652ca4
Alexander Sokol <sokol.kh@gmail.com>
2020-10-19 00:34:12 +0300

    add installer for thaum matrix

---
+++ src/thaum_matrix/install.lua
@@ -0,0 +1,5 @@
+shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/aspect/aspect.lua /home/aspect.lua")
+shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/crafting_state.lua /home/crafting_state.lua")
+shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/master.lua /home/master.lua")
+shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/options.lua /home/options.lua")
+shell.execute("wget https://raw.githubusercontent.com/alexandersokol/oc-home-apps/master/src/thaum_matrix/recipes.lua /home/recipes.lua")
